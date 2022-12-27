e4ca903c1ce7cc20aa11adaccf4a526706ad3203
DemoNataliia <nataliam110388@gmail.com>
2022-12-27 13:37:45 +0200

    добавила перевірки

--- src/api/index.js
+++ src/api/index.js
@@ -1,29 +1,7 @@
 import axios from 'axios';
-import { createCard, addCards } from '../markup'
-import {refs} from '../refs'
-
-// const API_KEY = "32288406-b66d59f2ed44691f7d029918e";
-// const BASE_URL = "https://pixabay.com";
-
-
-
-// export async function getQuery(searchQuery = '', { per_page = 20, page = 1 } = {}) {
-//         const searchParams = new URLSearchParams({ per_page, page });
-//         console.log((searchParams).toString());
-   
-//   try {
-//     const response = await axios.get(`${BASE_URL}/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&${searchParams}`);
-   
-//           const markup = createCard(response.data.hits)
-//       addCards(refs.divGallery, markup)
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
 
 const API_KEY = "32288406-b66d59f2ed44691f7d029918e";
-const BASE_URL = "https://pixabay.com";
+axios.defaults.baseURL = 'https://pixabay.com';
 
 
 export class NewsApiService {
@@ -32,21 +10,16 @@ export class NewsApiService {
       this.page = 1;
   }
     
-    async getQuery(searchQuery) {
+    async getData(searchQuery) {
   try {
-    const response = await axios.get(`${BASE_URL}/api/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&${this.page}`);
-   
-    const markup = createCard(response.data.hits)
-      addCards(refs.divGallery, markup)
-       this.incrementPage();
+      const response = await axios.get(`/api/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&${this.page}`);
+      if(response.data.total === 0)  throw new Error (response.statusText)
+      return response
   } catch (error) {
-    console.error(error);
+   throw new Error (error)
   }
     }
     
-    addBtn(){
-  refs.loadMore.classList.remove("is-hidden");
-};
 
   incrementPage() {
     this.page += 1;
@@ -56,11 +29,4 @@ export class NewsApiService {
     this.page = 1;
   }
 
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
 }
--- src/main.js
+++ src/main.js
@@ -1,41 +1,61 @@
-import {refs} from './refs'
 import SimpleLightbox from "simplelightbox";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import "simplelightbox/dist/simple-lightbox.min.css";
-import {NewsApiService} from './api'
+import {refs} from './refs'
+import { NewsApiService } from './api'
+import { createCard, addCards } from './markup'
+
+
+Notify.init({
+    width: '350px',
+    position: 'center-center',
+    fontSize: '20px',
+});
 
 const newsApiService = new NewsApiService()
 
 refs.form.addEventListener('submit', onSearch)
 refs.loadMore.addEventListener('click', onLoadBtn)
 
-function onSearch(e) {
+async function onSearch(e) {
     e.preventDefault()
 
-    newsApiService.addBtn();
+    addBtn();
     clearContainer()
     newsApiService.resetPage()
-     
-Array.from(e.target.elements).forEach(({value}) => {
-    if (value) {
-        newsApiService.query = value
-        newsApiService.getQuery(this.query)  
-    }
-});
-}
-
-function clearContainer() {
-  refs.divGallery.innerHTML = "";
+    try {
+        if (e.target.searchQuery.value === '') return
+newsApiService.searchQuery = e.target.searchQuery.value.trim()
+         const response = await newsApiService.getData(this.searchQuery)  
+         const markup = createCard(response.data.hits)
+      addCards(refs.divGallery, markup)
+            
+    } catch (error) {
+       Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        }
 }
 
 // let gallery = new SimpleLightbox('.gallery a');
 
 
-function onLoadBtn(e){
-    newsApiService.getQuery()
+async function onLoadBtn(e){
+    try {
+    newsApiService.incrementPage();
+         const response = await newsApiService.getData(this.searchQuery)  
+         const markup = createCard(response.data.hits)
+      addCards(refs.divGallery, markup)
+} catch (error) {
+    console.log(error);
+}
 };
 
+function clearContainer() {
+  refs.divGallery.innerHTML = "";
+}
 
-
+   function addBtn(){
+  refs.loadMore.classList.remove("is-hidden");
+};
 
 
 
